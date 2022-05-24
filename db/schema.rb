b89c41d8100d950e17f9d76df5db98ddef64482d
Andrea Spiga <drespriz@gmail.com>
2022-05-24 14:12:49 +0200

    adds validations for user, toy, booking  model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_24_100712) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_24_105754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "price"
+    t.bigint "toy_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["toy_id"], name: "index_bookings_on_toy_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "toys", force: :cascade do |t|
+    t.text "description"
+    t.string "name"
+    t.integer "cuteness"
+    t.boolean "kid_friendly"
+    t.integer "price_daily"
+    t.string "location"
+    t.integer "soul_taking_chance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_toys_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_24_100712) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "toys"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "toys", "users"
 end
