fbd0f6755327aafdd83c057c222590c757ae0a4c
Florian Bartel <bartel.florian@live.de>
2022-05-25 13:16:05 +0200

    added controller action destory for toy and solved pundit issues

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :toys, only: %i[new create show]
+  delete 'toys/:id', to: 'toys#destroy', as: 'delete_toy'
   # get 'toys/:id', to: "toys#show", as: :toy
   # get 'toys/new'
   # get 'toys/create'
