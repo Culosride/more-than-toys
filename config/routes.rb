cc2ee5d55744740c5ec1b29827219c4aee8202af
Sebastian Landucci <sebastianlanducci@gmail.com>
2022-05-25 16:01:25 +0200

    added banner

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root "toys#index"
-
+  #root "toys#index"
+  root to: 'pages#home'
   devise_for :users
   resources :toys, only: %i[new create show]
   # get 'toys/:id', to: "toys#show", as: :toy
   # get 'toys/new'
   # get 'toys/create'
+
 end
