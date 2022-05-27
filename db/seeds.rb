38f4865d658ac1a42ec5228b3470794bf114e01a
Florian Bartel <bartel.florian@live.de>
2022-05-27 11:59:56 +0200

    changed the style, added rating and kid friendliness

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,19 +22,32 @@ puts 'Finished!'
 
 puts 'Creating 10 fake toys...'
 
+#if you want to change names --> change photo name too. Also make sure chars are < 15 otherwise no validation
+names = ["Baby",
+  "The Accident",
+   "Car",
+   "Ouja Barbie",
+   "Meat Market",
+   "Skinny Bones",
+   "Teddy",
+   "Pink Thing",
+   "Crazy Ape",
+   "Satanic Priest"
+  ]
+
 10.times do
   toy = Toy.new(
-    name: Faker::Creature::Animal.name,
+    name: names.sample,
     description: "A beautiful harmless toy for your kids",
     # photo: File.open(File.join(Rails.root, "/app/assets/images/toys/#{rand(1..7)}.jpeg")),
-    cuteness: rand(5..10),
+    cuteness: rand(1..5),
     kid_friendly: true,
     price_daily: rand(1..99),
     address: Toy::CITIES.sample,
-    soul_taking_chance: rand(95..100),
+    soul_taking_chance: rand(1..99),
     user_id: @user[:id]
   )
-  toy.photo.attach(io: File.open(Rails.root.join("app/assets/images/toys/#{rand(1..10)}.jpeg")),
+  toy.photo.attach(io: File.open(Rails.root.join("app/assets/images/toys/#{toy.name}.jpeg")),
                   filename: 'toy.jpeg')
   toy.save!
 end
